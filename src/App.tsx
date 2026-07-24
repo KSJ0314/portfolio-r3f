1d6670f8b41517d1b27314476edd3814112163c5
김소중 <sojung017@gmail.com>
2026-07-24 14:12:07 +0900

    fix: 크레파스 스튜디오 여는 버튼을 배포 환경에서도 노출
    - App의 dev 게이트에서 CrayonStudio를 빼내 프로덕션에서도 여는 버튼이 뜨게 함
      (leva 튜닝 패널 DevHUD는 dev 전용 유지, 코드 좌표 복사도 dev로 가려진 채)

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,9 @@ function App() {
       <StationLifecycle />
       {/* 테마 토글은 밤 테마를 제대로 구현할 때 다시 단다(지금은 종이만 어두워진다). */}
       <Minimap />
-      {/* 개발용 도구(HUD 묶음 + 크레파스 스튜디오). dev에서만 렌더돼 프로덕션 번들에서 빠진다. */}
-      {import.meta.env.DEV && (
-        <>
-          <DevHUD />
-          <CrayonStudio />
-        </>
-      )}
+      {/* leva 튜닝 패널은 dev 전용이라 프로덕션 번들에서 빠진다. */}
+      {import.meta.env.DEV && <DevHUD />}
+      <CrayonStudio />
     </>
   )
 }
