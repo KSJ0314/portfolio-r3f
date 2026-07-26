5ddd5c381654c2212ecabe84f036839f93eb7da6
김소중 <sojung017@gmail.com>
2026-07-26 15:56:51 +0900

    refactor: 나가기 화살표를 전용 폴더로 분리
    - ExitArrow를 하위 폴더로 옮기고 획 좌표·기본 크기·색을 ExitArrow.constants.ts로,
      props 타입을 ExitArrow.types.ts로 분리
    - 페이지 배치(IntroPageLayout)는 구조를 그대로 두고 기본값만 화살표 상수에서 가져온다
    - 동작·값 변화 없음

---
+++ src/stations/sections/about/AboutIntro/ExitArrow/ExitArrow.types.ts
@@ -0,0 +1,10 @@
+/** 나가기 화살표 컴포넌트 props. */
+export interface ExitArrowProps {
+  x: number
+  y: number
+  size: number
+  color: string
+  stroke: number
+  roughness: number
+  opacity: number
+}
